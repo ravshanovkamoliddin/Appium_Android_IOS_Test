ffcca8257e59669dfe8e30f945cfaa7bde345609
Ravshanov Kamoliddin <rkamoliddin1991@gmail.com>
2024-12-10 11:44:09 +0500

    add eCommerce TC.1,TC.2

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -1,12 +1,11 @@
 import base.BaseTest;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 
-public class EcommerceTest extends BaseTest {
+public class ProductTest extends BaseTest {
 
 
     @Test
@@ -17,17 +16,23 @@ public class EcommerceTest extends BaseTest {
         driver.hideKeyboard();
         driver.findElement(By.xpath("//android.widget.RadioButton[@text= 'Female']")).click();
         driver.findElement(By.id("android:id/text1")).click();
-        /* country Uzbekistan */
-//        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Uzbekistan\"));"));
-//        driver.findElement(By.xpath("//android.widget.TextView[@text= 'Uzbekistan']")).click();
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
         driver.findElement(By.xpath("//android.widget.TextView[@text= 'Argentina']")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-        Thread.sleep(3000);
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"LeBron Soldier 12 \"));"));
 
-        /* Validation no send keys name
-        String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast) [1]")).getAttribute("name");
-        Assert.assertEquals(toastMessage,"Please enter your name"); */
+        int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+
+        for (int i = 0; i < productCount;i++) {
+            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+            if (productName.equalsIgnoreCase("LeBron Soldier 12 "))
+            {
+                driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).click();
+            }
+        }
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
+        Thread.sleep(3000);
 
     }
 }
